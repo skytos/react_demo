e25a98367a2c3ca762d14dab00b1b87ea2ba59b3
Skylar K Broad <skylar.broad@gmail.com>
2019-01-31 10:02:31 -0800

    handle increment button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,11 +5,15 @@ class Counter extends Component {
     count: 0
   }
 
+  handleIncrement = () => {
+    this.setState({count: this.state.count + 1});
+  }
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
