90f323331b113023b6264a929bb6028823f8d0e9
Skylar K Broad <skylar.broad@gmail.com>
2019-01-31 12:26:07 -0800

    make NavBar a stateless functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar <span className="badge badge-secondary badge-pill">{this.props.totalCounters}</span>
-        </a>
-      </nav>
-    );
-  }
+function NavBar(props) {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar <span className="badge badge-secondary badge-pill">{props.totalCounters}</span>
+      </a>
+    </nav>
+  );
 }
 
 export default NavBar;
