2f259513949e46fc288f17a1f94d91af737d2b5a
Skylar K Broad <skylar.broad@gmail.com>
2019-01-31 09:26:31 -0800

    add conditional class to the badge

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,10 +8,14 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span className="badge badge-primary m- 2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    return "badge m-2 badge-" + (this.state.count === 0 ? "warning" : "primary");
   }
 
   formatCount() {
