927400f27887b21fcc4f36027dc2792fa7dec261
Skylar K Broad <skylar.broad@gmail.com>
2019-01-31 11:01:45 -0800

    actually delete the counter

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,15 +11,16 @@ class Counters extends Component {
     ]
   }
 
-  handleDelete = () => {
-    console.log('delete from counters');
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
   }
 
   render() {
     return (
       <div>
         {this.state.counters.map(counter =>
-          <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} />)}
+          <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />)}
       </div>
     );
   }
